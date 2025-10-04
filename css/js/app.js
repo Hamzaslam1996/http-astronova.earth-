59c9030c707323c5ebfbdb02db9db89df7b3ec4a
Hamzaslam1996 <107784117+Hamzaslam1996@users.noreply.github.com>
2025-10-04 03:33:14 +0100

    Scaffold Space Apps website with judging sections
    Scaffolded the Space Apps website with sections for judging.

---
+++ css/js/app.js
@@ -0,0 +1,5 @@
+git add .
+git commit -m "Scaffold Space Apps website with judging-aligned sections"
+# if GitHub
+gh repo create Astronova-LEOmerce --public --source=. --remote=origin
+git push -u origin main
